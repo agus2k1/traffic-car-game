3f9bb2170cf4e2e270fdffa64ecb37cf1f8c0473
West <agus2kcoin@outlook.com>
2023-01-31 22:03:10 -0300

    Add canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css';
+import React, { Suspense, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useHelper } from '@react-three/drei';
+import Car from './Components/Car';
+import { DirectionalLightHelper } from 'three';
 
 function App() {
-  return <div className="App"></div>;
+  const light = useRef();
+  useHelper(light, DirectionalLightHelper, 'cyan');
+
+  return (
+    <div className="w-full h-full bg-purple-900 pt-10 pb-10">
+      <Canvas camera={{ position: [0, 0, 7] }}>
+        <OrbitControls />
+        <ambientLight visible={true} intensity={0.5} />
+        <directionalLight
+          visible={true}
+          position={[1, 1, 1]}
+          intensity={1}
+          color="blue"
+          ref={light}
+        />
+        <Suspense fallback={null}>
+          <Car />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
