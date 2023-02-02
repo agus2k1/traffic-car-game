2a29d74ca5d631aa4fb1f56e0f3e5e295949e260
West <agus2kcoin@outlook.com>
2023-02-01 22:09:12 -0300

    Add main car props

---
+++ src/assets/CarsData.js
@@ -0,0 +1,20 @@
+const redCarProps = {
+  wheels: {
+    frontWheelPosition: [18, 6, 0],
+    backWheelPosition: [-18, 6, 0],
+    wheelArgs: [12, 12, 33],
+    wheelColor: 0x333333,
+  },
+  main: {
+    mainPosition: [0, 12, 0],
+    mainArgs: [60, 15, 30],
+    mainColor: 0xa52523,
+  },
+  cabin: {
+    cabinPosition: [-6, 25.5, 0],
+    cabinArgs: [33, 12, 24],
+    cabinColor: 0xffffff,
+  },
+};
+
+export default redCarProps;
