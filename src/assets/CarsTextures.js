126ed02b985126f454d7c40d762fb664a7ef2c48
West <agus2kcoin@outlook.com>
2023-02-02 19:19:54 -0300

    Add truck textures

--- src/assets/CarsTextures.js
+++ src/assets/CarsTextures.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-export const getCarFrontTexture = () => {
+export const getPlayerCarFrontTexture = () => {
   const canvas = document.createElement('canvas');
   canvas.width = 64;
   canvas.height = 32;
@@ -15,7 +15,7 @@ export const getCarFrontTexture = () => {
   return new THREE.CanvasTexture(canvas);
 };
 
-export const getCarSideTexture = () => {
+export const getPlayerCarSideTexture = () => {
   const canvas = document.createElement('canvas');
   canvas.width = 128;
   canvas.height = 32;
@@ -27,6 +27,51 @@ export const getCarSideTexture = () => {
   ctx.fillStyle = '#666666';
   ctx.fillRect(10, 8, 38, 24);
   ctx.fillRect(58, 8, 60, 24);
+
+  return new THREE.CanvasTexture(canvas);
+};
+
+export const getTruckFrontTexture = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 64;
+  canvas.height = 32;
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#d6d238';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = '#444444';
+  ctx.fillRect(0, 5, canvas.width, 7);
+
+  return new THREE.CanvasTexture(canvas);
+};
+
+export const getTruckLeftSideTexture = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 64;
+  canvas.height = 32;
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#d6d238';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = '#444444';
+  ctx.fillRect(canvas.width - 30, 5, 30, 7);
+
+  return new THREE.CanvasTexture(canvas);
+};
+
+export const getTruckRightSideTexture = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 64;
+  canvas.height = 32;
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#d6d238';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = '#444444';
+  ctx.fillRect(30, 5, -30, 7);
 
   return new THREE.CanvasTexture(canvas);
 };
