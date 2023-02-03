f222b9887f27954b62f6add5b729549160b6a8a8
West <agus2kcoin@outlook.com>
2023-02-03 17:02:22 -0300

    Add car states

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -8,9 +8,19 @@ export const useGameContext = () => {
 
 export const GameProvider = ({ children }) => {
   const [resetGame, setResetGame] = useState(true);
+  const [accelerate, setAccelerate] = useState(false);
+  const [decelerate, setDecelerate] = useState(false);
 
   return (
-    <GameContext.Provider value={{ resetGame }}>
+    <GameContext.Provider
+      value={{
+        resetGame,
+        accelerate,
+        decelerate,
+        setAccelerate,
+        setDecelerate,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
