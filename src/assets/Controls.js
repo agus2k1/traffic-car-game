33a3bb9f35c166a0b3c47d04ab8be6a232302ff8
West <agus2kcoin@outlook.com>
2023-02-03 16:58:16 -0300

    Feature car controls

---
+++ src/assets/Controls.js
@@ -0,0 +1,40 @@
+let accelerate = false;
+let decelerate = false;
+
+const controls = () => {
+  // Key down
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp' || e.key === 'w') {
+      if (!accelerate) {
+        accelerate = true;
+        return;
+      }
+    }
+    if (e.key === 'ArrowDown' || e.key === 's') {
+      if (!decelerate) {
+        decelerate = true;
+        return;
+      }
+    }
+    if (e.key === 'R' || e.key === 'r') {
+      setResetGame(true);
+      return;
+    }
+  });
+
+  // Key up
+  window.addEventListener('keyup', (e) => {
+    if (e.key === 'ArrowUp' || e.key === 'w') {
+      if (accelerate) {
+        accelerate = false;
+        return;
+      }
+    }
+    if (e.key === 'ArrowDown' || e.key === 's') {
+      if (decelerate) {
+        decelerate = false;
+        return;
+      }
+    }
+  });
+};
