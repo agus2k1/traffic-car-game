b5af6a7eab284edace42c08de0a7d98e433c8ac4
West <agus2kcoin@outlook.com>
2023-02-03 16:59:22 -0300

    Change file name

---
+++ src/assets/VehiclesData.js
@@ -0,0 +1,43 @@
+export const carProps = {
+  wheels: {
+    frontWheelPosition: [18, 6, 0],
+    backWheelPosition: [-18, 6, 0],
+    wheelArgs: [12, 12, 33],
+    wheelColor: 0x333333,
+  },
+  main: {
+    mainPosition: [0, 12, 0],
+    mainArgs: [60, 15, 30],
+    mainColor: 0xa52523,
+  },
+  cabin: {
+    cabinPosition: [-6, 25.5, 0],
+    cabinArgs: [33, 12, 24],
+    cabinColor: 0xebe9cc,
+  },
+};
+
+export const truckProps = {
+  wheels: {
+    frontWheelPosition: [35, 6, 0],
+    middleWheelPosition: [10, 6, 0],
+    backWheelPosition: [-35, 6, 0],
+    wheelArgs: [12, 12, 30.01],
+    wheelColor: 0x333333,
+  },
+  main: {
+    mainPosition: [-15, 20, 0],
+    mainArgs: [70, 30, 30],
+    mainColor: 0xebe9cc,
+  },
+  cabin: {
+    cabinPosition: [36, 17, 0],
+    cabinArgs: [21, 25, 30],
+    cabinColor: 0xe3da36,
+  },
+  connector: {
+    connectorPosition: [23, 7.5, 0],
+    connectorArgs: [14, 5.3, 24],
+    connectorColor: 0xebe9cc,
+  },
+};
