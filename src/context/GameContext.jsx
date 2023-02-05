7f7a8520aac87c44c2f7f5a61b32c82833b8e1ad
West <agus2kcoin@outlook.com>
2023-02-04 23:23:05 -0300

    Add start/reset functions

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { carProps } from '../assets/VehiclesData';
+import { controls } from '../assets/Controls';
 
 const GameContext = React.createContext(null);
 
@@ -6,19 +9,39 @@ export const useGameContext = () => {
   return useContext(GameContext);
 };
 
+const vehiclesInitialState = [
+  {
+    name: 'greenCar',
+    type: 'car',
+    props: carProps,
+    color: 0x1f9c32,
+    position: [-160, 0, 220],
+  },
+];
 export const GameProvider = ({ children }) => {
-  const [resetGame, setResetGame] = useState(true);
-  const [accelerate, setAccelerate] = useState(false);
-  const [decelerate, setDecelerate] = useState(false);
+  const [ready, setReady] = useState(true);
+  const [otherVehicles, setOtherVehicles] = useState(vehiclesInitialState);
+
+  const reset = () => {
+    setOtherVehicles(vehiclesInitialState);
+  };
+
+  const startGame = () => {
+    if (ready) {
+      setReady(false);
+      // setAnimationLoop(animation);
+    }
+  };
+
+  controls();
 
   return (
     <GameContext.Provider
       value={{
-        resetGame,
-        accelerate,
-        decelerate,
-        setAccelerate,
-        setDecelerate,
+        ready,
+        otherVehicles,
+        setReady,
+        setOtherVehicles,
       }}
     >
       {children}
