5e4670423c7a9b9437d731240436cc7b31fefd67
West <agus2kcoin@outlook.com>
2023-02-06 19:18:49 -0300

    Remove states

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,6 +1,4 @@
 import React, { useContext, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { carProps, truckProps } from '../assets/VehiclesData';
 import { controls } from '../assets/Controls';
 
 const GameContext = React.createContext(null);
@@ -9,21 +7,8 @@ export const useGameContext = () => {
   return useContext(GameContext);
 };
 
-const vehiclesInitialState = [
-  {
-    name: 'car-1',
-    type: 'car',
-    props: carProps,
-  },
-  {
-    name: 'truck-1',
-    type: 'truck',
-    props: truckProps,
-  },
-];
 export const GameProvider = ({ children }) => {
   const [ready, setReady] = useState(true);
-  const [otherVehicles, setOtherVehicles] = useState(vehiclesInitialState);
 
   const reset = () => {
     setOtherVehicles(vehiclesInitialState);
@@ -32,7 +17,6 @@ export const GameProvider = ({ children }) => {
   const startGame = () => {
     if (ready) {
       setReady(false);
-      // setAnimationLoop(animation);
     }
   };
 
@@ -42,9 +26,7 @@ export const GameProvider = ({ children }) => {
     <GameContext.Provider
       value={{
         ready,
-        otherVehicles,
         setReady,
-        setOtherVehicles,
       }}
     >
       {children}
