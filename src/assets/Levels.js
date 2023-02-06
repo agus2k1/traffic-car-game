e4b7666c5abeabef413318f58f141d447d9faf6a
West <agus2kcoin@outlook.com>
2023-02-06 19:14:39 -0300

    Feature levels

---
+++ src/assets/Levels.js
@@ -0,0 +1,32 @@
+import { carProps, truckProps } from './VehiclesData';
+
+export const vehiclesLv1 = [
+  {
+    name: 'car-1',
+    type: 'car',
+    props: carProps,
+  },
+  {
+    name: 'truck-1',
+    type: 'truck',
+    props: truckProps,
+  },
+];
+
+export const vehiclesLv2 = [
+  {
+    name: 'car-1',
+    type: 'car',
+    props: carProps,
+  },
+  {
+    name: 'car-2',
+    type: 'car',
+    props: carProps,
+  },
+  {
+    name: 'truck-1',
+    type: 'truck',
+    props: truckProps,
+  },
+];
