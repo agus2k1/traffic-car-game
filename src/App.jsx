1471313dc5ccfa2cfd23459a933237b5ff44fcf3
West <agus2kcoin@outlook.com>
2023-02-10 19:02:53 -0300

    Add message/animation

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,51 @@ function App() {
   const cameraWidth = 2000;
   const cameraHeight = cameraWidth / aspectRatio;
 
-  const { resetGame } = useGameContext();
-
   return (
-    <div className="w-full h-full bg-purple-900 ">
-      <Canvas
-        orthographic
-        camera={{
-          position: [0, 300, -210],
-          aspect: aspectRatio,
-          left: cameraWidth / -2,
-          right: cameraWidth / 2,
-          top: cameraHeight / 2,
-          bottom: cameraHeight / -2,
-          near: 0,
-          far: 1000,
-          zoom: 1.2,
-        }}
-      >
-        {/* <gridHelper args={[10, 10, 'blue', 'black']} rotation={[0, 0, 0]} /> */}
-        <OrbitControls />
-        <Lights />
-        <Suspense fallback={null}>
-          <Cars />
-          <Map mapWidth={cameraWidth} mapHeight={cameraHeight * 2} />
-        </Suspense>
-      </Canvas>
-    </div>
+    <>
+      {/* <div className="message-wrapper">
+        <p className="message-title">You crashed!</p>
+        <p className="message-desc">Press R to restart</p>
+      </div> */}
+      <div className="game">
+        <Canvas
+          orthographic
+          camera={{
+            position: [0, 300, -210],
+            aspect: aspectRatio,
+            left: cameraWidth / -2,
+            right: cameraWidth / 2,
+            top: cameraHeight / 2,
+            bottom: cameraHeight / -2,
+            near: 0,
+            far: 1000,
+            zoom: 0.9,
+          }}
+          className="game-canvas"
+        >
+          <Lights />
+          <OrbitControls />
+          <Suspense fallback={null}>
+            <Cars />
+            <Map mapWidth={cameraWidth} mapHeight={cameraHeight * 2} />
+          </Suspense>
+        </Canvas>
+      </div>
+      <div className="page hidden">
+        <div className="page-wrapper">
+          <section className="hero">
+            <button>Animacion</button>
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+{
+  /* <video autoPlay muted loop className="video">
+            <source src="CarAnimation.mp4" type="video/mp4" />
+          </video> */
+}
