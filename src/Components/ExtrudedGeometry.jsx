7dd7834c5632f805be0f0a3e8b3763647dd1b29a
West <agus2kcoin@outlook.com>
2023-02-10 19:04:28 -0300

    Change extrude depth

--- src/Components/ExtrudedGeometry.jsx
+++ src/Components/ExtrudedGeometry.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function ExtrudedGeometry({ shape }) {
+const ExtrudedGeometry = ({ shape }) => {
   return (
-    <mesh rotation={[-1.57, 0, 0]}>
+    <mesh rotation={[-1.57, 0, 0]} onPointerEnter={() => console.log('click')}>
       <extrudeGeometry
         attach="geometry"
-        args={[shape, { depth: 6, bevelEnabled: false }]}
+        args={[shape, { depth: 8, bevelEnabled: false }]}
       />
       <meshLambertMaterial attach="material" color={0x2d800e} />
     </mesh>
   );
-}
+};
 
 export default ExtrudedGeometry;
