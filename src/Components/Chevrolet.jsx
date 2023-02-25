b55d1ac1c314faf40c7a3e43f8b739e70feca492
West <agus2kcoin@outlook.com>
2023-02-25 19:50:09 -0300

    Feature chevrolet logo

---
+++ src/Components/Chevrolet.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useGLTF } from '@react-three/drei';
+
+const Chevrolet = () => {
+  const { nodes, materials } = useGLTF('/chevrolet-logo.glb');
+  return (
+    <group dispose={null}>
+      <group position={[-450, 11, -300]} scale={[30, 10, 30]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane.geometry}
+          material={materials['Material.001']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane_1.geometry}
+          //   material={materials['Material.002']}
+        >
+          <meshLambertMaterial color={'#545454'} />
+        </mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane_2.geometry}
+          material={materials['Material.003']}
+        />
+      </group>
+    </group>
+  );
+};
+
+useGLTF.preload('/chevrolet-logo.glb');
+
+export default Chevrolet;
