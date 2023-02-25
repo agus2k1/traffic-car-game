19d2d6abc4e302e52641ba7d06b47f08021c3eeb
West <agus2kcoin@outlook.com>
2023-02-24 21:06:44 -0300

    Add plane field

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import ExtrudedGeometry from './ExtrudedGeometry';
 import {
   getLineMarkings,
   getLeftIsland,
   getRightIsland,
   getMiddleIsland,
   getOuterField,
+  getField,
 } from '../assets/MapTexture';
-import ExtrudedGeometry from './ExtrudedGeometry';
+import { useGameContext } from '../context/GameContext';
 
 const Map = ({ mapWidth, mapHeight }) => {
+  const { displayCars } = useGameContext();
+
   const lineMarkingsTexture = getLineMarkings(mapWidth, mapHeight);
 
   // Extruded geometry
@@ -16,17 +20,26 @@ const Map = ({ mapWidth, mapHeight }) => {
   const rightIsland = getRightIsland();
   const middleIsland = getMiddleIsland();
   const outerField = getOuterField(mapWidth, mapHeight);
+  const field = getField(mapWidth, mapHeight);
 
   return (
     <>
-      <mesh rotation={[-1.57, 0, 0]} position={[0, 0, 0]}>
-        <planeGeometry attach="geometry" args={[mapWidth, mapHeight]} />
-        <meshLambertMaterial attach="material" map={lineMarkingsTexture} />
-      </mesh>
-      <ExtrudedGeometry shape={leftIsland} />
-      <ExtrudedGeometry shape={rightIsland} />
-      <ExtrudedGeometry shape={middleIsland} />
-      <ExtrudedGeometry shape={outerField} />
+      {displayCars ? (
+        <group>
+          <ExtrudedGeometry shape={field} />
+        </group>
+      ) : (
+        <group>
+          <mesh rotation={[-1.57, 0, 0]} position={[0, 0, 0]}>
+            <planeGeometry attach="geometry" args={[mapWidth, mapHeight]} />
+            <meshLambertMaterial attach="material" map={lineMarkingsTexture} />
+          </mesh>
+          <ExtrudedGeometry shape={leftIsland} />
+          <ExtrudedGeometry shape={rightIsland} />
+          <ExtrudedGeometry shape={middleIsland} />
+          <ExtrudedGeometry shape={outerField} />
+        </group>
+      )}
     </>
   );
 };
