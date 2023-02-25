1fe429449dc33aafba378a0f7454e2de988652ec
West <agus2kcoin@outlook.com>
2023-02-25 19:50:55 -0300

    Feature logos

---
+++ src/Components/Logos.jsx
@@ -0,0 +1,13 @@
+import Chevrolet from './Chevrolet';
+import Mitsubishi from './Mitsubishi';
+
+const Logos = () => {
+  return (
+    <group>
+      <Chevrolet />
+      <Mitsubishi />
+    </group>
+  );
+};
+
+export default Logos;
