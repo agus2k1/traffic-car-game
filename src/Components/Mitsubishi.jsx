aef10938cdc562cd840d86725a4fa1ede6c5b198
West <agus2kcoin@outlook.com>
2023-02-27 20:22:58 -0300

    Add position prop

--- src/Components/Mitsubishi.jsx
+++ src/Components/Mitsubishi.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useGLTF } from '@react-three/drei';
 
-const Mitsubishi = () => {
+const Mitsubishi = ({ position }) => {
   const { nodes, materials } = useGLTF('/mitsubishi-logo.glb');
   return (
     <group
       dispose={null}
       scale={50}
-      position={[0, 11, -300]}
+      position={[position, 11, -300]}
       rotation={[0, 1.04, 0]}
     >
       <mesh
