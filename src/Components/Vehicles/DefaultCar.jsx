6683258392ffd14194b18c16b6baf13ac849e164
West <agus2kcoin@outlook.com>
2023-02-28 20:01:03 -0300

    Add name prop

--- src/Components/Vehicles/DefaultCar.jsx
+++ src/Components/Vehicles/DefaultCar.jsx
@@ -30,25 +30,26 @@ const DefaultCar = ({
       position={position}
       rotation={rotation}
       userData={{ index, playerScore, angleMoved, active }}
-      onPointerEnter={() => console.log('asd')}
     >
       {/* Back Wheel */}
-      <mesh position={backWheelPosition}>
+      <mesh position={backWheelPosition} name={'back-wheels'}>
         <boxGeometry attach="geometry" args={wheelArgs} />
         <meshLambertMaterial color={wheelColor} />
       </mesh>
       {/* Front Wheel */}
-      <mesh position={frontWheelPosition}>
+      <mesh position={frontWheelPosition} name={'front-wheels'}>
         <boxGeometry attach="geometry" args={wheelArgs} />
         <meshLambertMaterial color={wheelColor} />
       </mesh>
       {/* Main */}
-      <mesh position={mainPosition}>
-        <boxGeometry attach="geometry" args={mainArgs} />
-        <meshLambertMaterial color={color} />
-      </mesh>
+      <group name={'main'}>
+        <mesh position={mainPosition}>
+          <boxGeometry attach="geometry" args={mainArgs} />
+          <meshLambertMaterial color={color} />
+        </mesh>
+      </group>
       {/* Cabin */}
-      <mesh position={cabinPosition}>
+      <mesh position={cabinPosition} name={'cabin'}>
         <boxGeometry attach="geometry" args={cabinArgs} />
         <meshLambertMaterial attach="material-0" map={getCarFrontTexture()} />
         <meshLambertMaterial attach="material-1" map={getCarFrontTexture()} />
